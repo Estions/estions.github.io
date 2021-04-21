eea65d471c527a1cdda92d6873a40f61c2680d9f
Estions <36627805+Estions@users.noreply.github.com>
2021-04-21 21:51:30 +0300

    Yes

--- js/page-layout.js
+++ js/page-layout.js
@@ -4,21 +4,25 @@ let footer = document.querySelector(".bottom");
 let body = document.getElementById("body");
 
 const observer = new IntersectionObserver(function(entries, observer){
-    entries.forEach(entry => {
-        if(entry.target == header && entry.isIntersecting){ //When Scrolling to Landing Page
-            body.classList.remove("show-main");
-            console.log("top");
-        }
-        if(entry.target == footer && entry.isIntersecting){ //When scrolling to main
-            body.classList.add("show-main");
-            console.log("bottom");
-        }
-    })
-},{
+       entries.forEach(entry => {
+            if(entry.target == header && entry.isIntersecting){ //When Scrolling to Landing Page
+                body.classList.remove("show-main");
+            }
+            if(entry.target == footer && entry.isIntersecting){ //When scrolling to main
+                changeScreenMain();
+            }
+        })
+    }
+,{
     root: null,
     rootMargin: '0px',
     threshold: 1
 });
+
+function changeScreenMain(){
+    setTimeout(function(){scrollTo(0,header.scrollHeight)},750);
+    body.classList.add("show-main");
+}
 
 observer.observe(header);
 observer.observe(footer);
